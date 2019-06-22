3798145749c3ca0be8f14e609374a7315f44ab44
Puspa Sharma <blackice002@gmail.com>
2019-06-22 11:31:06 -0400

    recipe finder and resources page components updated

---
+++ client/src/components/Resource/ImgCarousel.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+} from 'reactstrap';
+
+const items = [
+  {
+    src: "../images/preIMg6.jpg",
+    altText: 'Slide 1',
+  },
+  {
+    src: "../images/preIMg5.jpg",
+    altText: 'Slide 2',
+  },
+  {
+    src:"../images/preIMg2.png" ,
+    altText: 'Slide 3',
+  },
+  {
+    src:"../images/preIMg9.jpg" ,
+    altText: 'Slide 4',
+  
+  }
+];
+
+class ImgCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <img src={item.src}  alt={item.altText} className="carouselImg" />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl className="arrowColor" direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl className="arrowColor" direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
+}
+
+
+export default ImgCarousel;
