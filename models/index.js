baa1670c1c142e3fc95fe26834879db7702ecdf6
Rakim francis <rakimfrancis@dyn-209-2-225-208.dyn.columbia.edu>
2019-06-24 20:01:04 -0400

    working version

--- models/index.js
+++
@@ -1,8 +0,0 @@
-// Exporting an object containing all of our models
-
-module.exports = {
-    User: require("./User"),
-    Calorie: require("./Calorie"),
-    MedId: require("./MedId")
-  };
-  
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const calorieRoutes = require("./calorie");
-
-// Book routes
-router.use("/calorie", calorieRoutes);
-
-module.exports = router;
